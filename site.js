2ee19dedd008c1fabeb44adf168d0f83adf0c7bb
Valerija K <lerakrastinja@gmail.com>
2018-06-18 20:53:00 +0200

    Initial commit

---
+++ site.js
@@ -0,0 +1,27 @@
+$( document ).ready(function() {
+    
+    // This code is called when jquery is ready to be used
+    $.getJSON( "https://api.detskeriaarhus.dk/api/events", function( eventsData ) {
+        console.log(eventsData);
+
+        var source = $("#events-list-template").html(); 
+        var template = Handlebars.compile(source); 
+
+        var data = {};
+        data.events = eventsData;
+
+        Handlebars.registerHelper('isFree', function(block) {
+            if (Array.isArray(this.customTags) && this.customTags.indexOf('Gratis') >= 0) {
+                return block.fn(this); // pokazatj to chto vnutri v HTML
+            }
+            else{
+                return block.inverse(this); // nechevo ne pokazatj
+            }
+
+        });
+
+        $('.container').append(template(data));
+      });
+    
+  });
+
